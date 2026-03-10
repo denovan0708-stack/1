e63c4887daf9af860f7a193e236c7bf5f268af76
denovan0708-stack <denovan0708@gmail.com>
2026-03-10 15:25:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdownBox = document.getElementById("countdown-container");
-const targetDate = new Date("March 10, 2026 15:23:00").getTime();
+const targetDate = new Date("March 1, 2026 15:23:00").getTime();
 let particleInterval;
 let stopAllParticles = false;
 
@@ -158,4 +158,5 @@ window.addEventListener('touchmove', (e) => {
     countdownBox.style.setProperty('--cursor-x', e.touches[0].clientX + 'px');
     countdownBox.style.setProperty('--cursor-y', e.touches[0].clientY + 'px');
 });
+
 
